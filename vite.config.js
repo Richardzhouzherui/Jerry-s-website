e606f8e1d16c01732f4fb3f34a4b8e21ebea814b
Jerry Zhou <jerryzhou@Jerry-Zhou.local>
2026-02-25 02:36:48 +0800

    Fix deployment path and config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Jerry-s-website/' : '/',
-  server: {
-    port: 5173,
-    // 添加允许访问的 Ngrok 域名
-    allowedHosts: [
-      "retroserrate-jeffry-unconfusedly.ngrok-free.dev", // 你当前 ngrok 生成的域名
-      ".ngrok-free.dev", // 通配，避免每次启动都改
-    ]
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/Jerry-s-website/' : '/',
+    server: {
+      port: 5173,
+      allowedHosts: [
+        "retroserrate-jeffry-unconfusedly.ngrok-free.dev",
+        ".ngrok-free.dev",
+      ]
+    }
   }
 })
